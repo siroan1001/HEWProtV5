89e1c4088530605d22564d6f7cc02ba870b001bf
ikouki1001 <2002gg1001@gmail.com>
2023-01-27 03:47:55 +0900

    ゴーストモデル置き換え 描画されないから調整必要

--- DX22Base/ChasingShadow.cpp
+++ DX22Base/ChasingShadow.cpp
@@ -1,13 +1,124 @@
 #include "ChasingShadow.h"
+#include "ModelList.h"
+
+//ChasingShadow::ChasingShadow()
+//	:m_Info{ {-10.0f, 5.0f, 0.0f}, {2.0f, 0.5f, 2.0f}, {XMConvertToRadians(90.0f), 0.0f, 0.0f} }
+//	, m_PlDirection(Collision::E_DIRECTION_L)
+//{
+//}
+//
+//ChasingShadow::~ChasingShadow()
+//{
+//}
+//
+//void ChasingShadow::Update()
+//{
+//	const int cn_MaxFlame = 300;
+//	m_PosLog.push_back(m_pPlayer->GetInfo().pos);
+//
+//	if (m_PosLog.size() >= cn_MaxFlame)
+//	{
+//		m_Info.pos = *m_PosLog.begin();
+//		m_PosLog.pop_front();
+//	}
+//	/*const float SHADOW_MOVE = 0.01f;
+//	const float LIMIT = 3.0f;
+//
+//	Collision::Direction PlOldDirection = m_PlDirection;
+//	m_PlDirection = m_pPlayer->GetDirection();
+//
+//	XMFLOAT3 PlayerPos = m_pPlayer->GetInfo().pos;
+//
+//	if (PlOldDirection != m_PlDirection)
+//	{
+//		switch (m_PlDirection)
+//		{
+//		case Collision::E_DIRECTION_NULL:
+//			break;
+//		case Collision::E_DIRECTION_L:
+//			m_Info.pos.x = PlayerPos.x - LIMIT;
+//			m_Info.pos.y = PlayerPos.y;
+//			break;
+//		case Collision::E_DIRECTION_R:
+//			m_Info.pos.x = PlayerPos.x + LIMIT;
+//			m_Info.pos.y = PlayerPos.y;
+//			break;
+//		case Collision::E_DIRECTION_U:
+//			break;
+//		case Collision::E_DIRECTION_D:
+//			break;
+//		case Collision::E_DIRECTION_MAX:
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+//
+//
+//
+//
+//	if (PlayerPos.x > m_Info.pos.x)
+//	{
+//		m_Info.pos.x += SHADOW_MOVE;
+//	}
+//	if (PlayerPos.x < m_Info.pos.x)
+//	{
+//		m_Info.pos.x -= SHADOW_MOVE;
+//	}
+//	if (PlayerPos.y > m_Info.pos.y)
+//	{
+//		m_Info.pos.y += SHADOW_MOVE * 0.5f;
+//	}
+//	if (PlayerPos.y < m_Info.pos.y)
+//	{
+//		m_Info.pos.y -= SHADOW_MOVE * 0.5f;
+//	}
+//	if (m_Info.pos.x < PlayerPos.x - LIMIT)
+//	{
+//		m_Info.pos.x  = PlayerPos.x - LIMIT;
+//	}
+//	if (m_Info.pos.x > PlayerPos.x + LIMIT)
+//	{
+//		m_Info.pos.x = PlayerPos.x + LIMIT;
+//	}*/
+//}
+//
+//void ChasingShadow::Draw()
+//{
+//	SetGeometoryTranslate(m_Info.pos.x, m_Info.pos.y, m_Info.pos.z);
+//	SetGeometoryScaling(m_Info.size.x, m_Info.size.y, m_Info.size.z);
+//	SetGeometoryRotation(m_Info.rot.x, m_Info.rot.y, m_Info.rot.z);
+//	DrawCylinder();
+//}
+//
+//void ChasingShadow::SetPlayer(Player * pPlayer)
+//{
+//	m_pPlayer = pPlayer;
+//}
+//
+//Def::Info ChasingShadow::GetInfo()
+//{
+//	return m_Info;
+//}
+//
+//float ChasingShadow::GetRadius()
+//{
+//	return GetCylinderRadius() * m_Info.size.x;
+//}
 
 ChasingShadow::ChasingShadow()
-	:m_Info{ {-10.0f, 5.0f, 0.0f}, {2.0f, 0.5f, 2.0f}, {XMConvertToRadians(90.0f), 0.0f, 0.0f} }
-	, m_PlDirection(Collision::E_DIRECTION_L)
+	:m_PlDirection(Collision::E_DIRECTION_L)
 {
-}
+	m_ModelSize.x = m_ModelSize.y = m_ModelSize.z = 0.03f;
+	m_pModel = ModelList::GetModel(ModelList::E_MODEL_LIST_CONST_GHOST);
 
-ChasingShadow::~ChasingShadow()
-{
+	//頂点シェーダをモデルに設定
+	m_pModel->SetVertexShader(m_pVS);
+	m_pModel->SetPixelShader(m_pPS);
+
+	m_Info = { {-10.0f, 5.0f, 0.0f}, {2.0f, 0.5f, 2.0f}, {0.0f, -90.0f, 0.0f} };
+
+
 }
 
 void ChasingShadow::Update()
@@ -82,25 +193,12 @@ void ChasingShadow::Update()
 	}*/
 }
 
-void ChasingShadow::Draw()
-{
-	SetGeometoryTranslate(m_Info.pos.x, m_Info.pos.y, m_Info.pos.z);
-	SetGeometoryScaling(m_Info.size.x, m_Info.size.y, m_Info.size.z);
-	SetGeometoryRotation(m_Info.rot.x, m_Info.rot.y, m_Info.rot.z);
-	DrawCylinder();
-}
-
 void ChasingShadow::SetPlayer(Player * pPlayer)
 {
 	m_pPlayer = pPlayer;
 }
 
-Def::Info ChasingShadow::GetInfo()
-{
-	return m_Info;
-}
-
 float ChasingShadow::GetRadius()
 {
-	return GetCylinderRadius() * m_Info.size.x;
+	return 2.0f;
 }
--- DX22Base/LayerGame.cpp
+++ DX22Base/LayerGame.cpp
@@ -188,7 +188,7 @@ void LayerGame::Draw()
 	m_pStartObj->Draw();
 
 	//追ってくる影の描画
-	//m_pChasingShadow->Draw();
+	m_pChasingShadow->Draw();
 	
 	m_pGoalObj->Draw();
 
@@ -450,10 +450,10 @@ void LayerGame::CheckCollision()
 	}
 
 	//プレイヤーと追ってくる影
-	//if (Collision::RectAndCircle(m_pPlayer->GetInfo(), m_pChasingShadow->GetInfo(), m_pChasingShadow->GetRadius()))
-	//{
-	//	SceneGame::SetGameStatus(SceneGame::E_GAME_STATUS_GAMEOVER);
-	//}
+	if (Collision::RectAndCircle(m_pPlayer->GetInfo(), m_pChasingShadow->GetInfo(), m_pChasingShadow->GetRadius()))
+	{
+		SceneGame::SetGameStatus(SceneGame::E_GAME_STATUS_GAMEOVER);
+	}
 
 	//プレイヤーとスタート板
 	if (*m_GameStatus == SceneGame::E_GAME_STATUS_START)
