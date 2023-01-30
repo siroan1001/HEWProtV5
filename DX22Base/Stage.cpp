75e204870528b2ae965f12589bde6220fdace87a
ikouki1001 <2002gg1001@gmail.com>
2023-01-31 06:19:36 +0900

    ステージ２．３修正 一部不具合あり

--- DX22Base/Stage.cpp
+++ DX22Base/Stage.cpp
@@ -15,7 +15,7 @@ Stage::Stage()
 
 	m_pStageNum = SceneStageSelect::GetStageNumber();
 	
-	//ステージ１
+	//ステージ1
 	//床
 	info.pos = { 1.6f, 3.0f, 0.0f };
 	info.size = { 22.0f, 0.5f, 0.5f };
@@ -42,7 +42,7 @@ Stage::Stage()
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_1].m_Info.push_back(info);
 
-	info.pos = { 6.5f, 5.275f, 0.0f };
+	info.pos = { 6.5f, 5.075f, 0.0f };
 	info.size = { 0.2f, 0.25f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_1].m_Info.push_back(info);
@@ -90,51 +90,100 @@ Stage::Stage()
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
 
-	//一番最初のシャドウブロック
-	info.pos = { -3.7f, 3.95f, 0.0f };
-	info.size = { 0.5f, 3.0f, 0.5f };
-	info.rot = { 0.0f, 0.0f, 0.0f };
-	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
-
-	//1つ目の台
-	info.pos = { -3.0f, 3.3f, 0.0f };
-	info.size = { 2.3f, 0.1f, 0.5f };
+	//壁
+	//1
+	info.pos = { -2.17f, 3.55f, 0.0f };
+	info.size = { 0.2f, 0.8f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
 
-	//二番目のシャドウブロック
-	info.pos = { -1.45f, 3.95f, 0.0f };
-	info.size = { 0.5f, 1.0f, 0.5f };
-	info.rot = { 0.0f, 0.0f, 0.0f };
-	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
-
-	//三番目のシャドウブロック
-	info.pos = { 2.5f, 4.25f, 0.0f };
-	info.size = { 0.8f, 1.0f, 0.5f };
-	info.rot = { 0.0f, 0.0f, 0.0f };
-	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
-
-	//2つ目の台
-	info.pos = { 3.5f, 3.3f, 0.0f };
-	info.size = { 2.0f, 0.1f, 0.5f };
+	//2
+	info.pos = { 1.6f, 3.78f, 0.0f };
+	info.size = { 0.2f, 1.0f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
 
-	//四番目のシャドウブロック
-	info.pos = { 5.4f, 4.25f, 0.0f };
+	//3
+	info.pos = { 2.58f, 4.8f, 0.0f };
+	info.size = { 0.2f, 0.8f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
+
+	//4
+	info.pos = { 2.58f, 3.55f, 0.0f };
+	info.size = { 0.2f, 0.8f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
+
+	//5
+	info.pos = { 3.73f, 4.8f, 0.0f };
+	info.size = { 0.5f, 0.7f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
+
+	//6
+	info.pos = { 3.73f, 3.6f, 0.0f };
+	info.size = { 0.5f, 0.7f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
+
+	//7
+	info.pos = { 6.5f, 4.3f, 0.0f };
+	info.size = { 0.5f, 0.7f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
+
+	//8
+	info.pos = { 7.78f, 3.98f, 0.0f };
+	info.size = { 0.5f, 0.25f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
+
+	//シャドウブロック1
+	info.pos = { -2.3f, 3.95f, 0.0f };
+	info.size = { 1.3f, 0.8f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック2
+	info.pos = { -0.8f, 3.95f, 0.0f };
+	info.size = { 1.3f, 0.8f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック3
+	info.pos = { 1.47f, 4.25f, 0.0f };
 	info.size = { 1.0f, 1.0f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
 
-	//3つ目の台
-	info.pos = { 6.2f, 3.4f, 0.0f };
-	info.size = { 3.5f, 0.12f, 0.5f };
+	//シャドウブロック4
+	info.pos = { 2.47f, 4.67f, 0.0f };
+	info.size = { 0.8f, 0.8f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
-	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Info.push_back(info);
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
 
-	//五番目のシャドウブロック
-	info.pos = { 8.7f, 4.25f, 0.0f };
-	info.size = { 0.8f, 1.0f, 0.5f };
+	//シャドウブロック5
+	info.pos = { 3.45f, 4.67f, 0.0f };
+	info.size = { 0.8f, 0.8f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック6
+	info.pos = { 4.75f, 4.65f, 0.0f };
+	info.size = { 0.8f, 1.4f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック7
+	info.pos = { 7.52f, 4.65f, 0.0f };
+	info.size = { 0.8f, 1.4f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック8
+	info.pos = { 8.8f, 4.65f, 0.0f };
+	info.size = { 0.8f, 0.8f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_2].m_Shadow.push_back(new ShadowBlock(info));
 
@@ -145,51 +194,136 @@ Stage::Stage()
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
 
-	//一番最初のシャドウブロック
-	info.pos = { -3.7f, 3.95f, 0.0f };
-	info.size = { 0.5f, 3.0f, 0.5f };
-	info.rot = { 0.0f, 0.0f, 0.0f };
-	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
-
-	//1つ目の台
-	info.pos = { -3.0f, 3.3f, 0.0f };
-	info.size = { 2.3f, 0.1f, 0.5f };
+	//壁
+	//1
+	info.pos = { -1.28f, 4.05f, 0.0f };
+	info.size = { 0.5f, 0.3f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
 
-	//二番目のシャドウブロック
-	info.pos = { -1.45f, 3.95f, 0.0f };
-	info.size = { 0.5f, 1.0f, 0.5f };
+	//2
+	info.pos = { -1.28f, 3.35f, 0.0f };
+	info.size = { 0.5f, 0.3f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//3
+	info.pos = { 0.0f, 3.85f, 0.0f };
+	info.size = { 0.5f, 0.3f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//4
+	info.pos = { 0.0f, 4.48f, 0.0f };
+	info.size = { 0.5f, 0.3f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//5
+	info.pos = { 1.28f, 4.35f, 0.0f };
+	info.size = { 0.5f, 0.6f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//6
+	info.pos = { 3.28f, 3.35f, 0.0f };
+	info.size = { 0.5f, 0.8f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//7
+	info.pos = { 4.7f, 3.35f, 0.0f };
+	info.size = { 0.8f, 0.55f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//8
+	info.pos = { 4.7f, 4.45f, 0.0f };
+	info.size = { 0.8f, 0.3f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//9
+	info.pos = { 5.48f, 4.15f, 0.0f };
+	info.size = { 0.8f, 0.3f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//10
+	info.pos = { 6.11f, 4.15f, 0.0f };
+	info.size = { 0.5f, 0.7f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//11
+	info.pos = { 7.38f, 4.05f, 0.0f };
+	info.size = { 0.5f, 0.7f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//12
+	info.pos = { 8.65f, 4.28f, 0.0f };
+	info.size = { 0.5f, 0.25f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+
+	//シャドウブロック1
+	info.pos = { -1.57f, 4.25f, 0.0f };
+	info.size = { 1.2f, 1.0f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
 
-	//三番目のシャドウブロック
-	info.pos = { 2.5f, 4.25f, 0.0f };
+	//シャドウブロック2
+	info.pos = { -0.25f, 4.41f, 0.0f };
 	info.size = { 0.8f, 1.0f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
 
-	//2つ目の台
-	info.pos = { 3.5f, 3.3f, 0.0f };
-	info.size = { 2.0f, 0.1f, 0.5f };
-	info.rot = { 0.0f, 0.0f, 0.0f };
-	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
-
-	//四番目のシャドウブロック
-	info.pos = { 5.4f, 4.25f, 0.0f };
-	info.size = { 1.0f, 1.0f, 0.5f };
+	//シャドウブロック3
+	info.pos = { 1.02f, 4.38f, 0.0f };
+	info.size = { 0.8f, 1.1f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
 
-	//3つ目の台
-	info.pos = { 6.2f, 3.4f, 0.0f };
-	info.size = { 3.5f, 0.12f, 0.5f };
+	//シャドウブロック4
+	info.pos = { 3.02f, 4.38f, 0.0f };
+	info.size = { 0.8f, 1.1f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
-	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Info.push_back(info);
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
 
-	//五番目のシャドウブロック
-	info.pos = { 8.7f, 4.25f, 0.0f };
-	info.size = { 0.8f, 1.0f, 0.5f };
+	//シャドウブロック5
+	info.pos = { 4.3f, 4.59f, 0.0f };
+	info.size = { 0.8f, 0.9f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック6
+	info.pos = { 5.07f, 4.3f, 0.0f };
+	info.size = { 0.8f, 0.7f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック7
+	info.pos = { 5.85f, 4.0f, 0.0f };
+	info.size = { 0.8f, 0.7f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック8
+	info.pos = { 5.85f, 4.97f, 0.0f };
+	info.size = { 0.8f, 0.7f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック9
+	info.pos = { 7.13f, 4.38f, 0.0f };
+	info.size = { 0.8f, 1.1f, 0.5f };
+	info.rot = { 0.0f, 0.0f, 0.0f };
+	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
+
+	//シャドウブロック10
+	info.pos = { 8.4f, 4.38f, 0.0f };
+	info.size = { 0.8f, 1.1f, 0.5f };
 	info.rot = { 0.0f, 0.0f, 0.0f };
 	m_Date[SceneGame::E_STAGE_NUMBER_STAGE_3].m_Shadow.push_back(new ShadowBlock(info));
 
